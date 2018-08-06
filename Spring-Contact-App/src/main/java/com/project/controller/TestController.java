931544c7f9ab7b382b84b12c48d79db39239c986
suresh waghamare <sureshwaghamare1@gmail.com>
2018-08-06 15:51:44 +0530

    Successfully Configuration Done!!

---
+++ Spring-Contact-App/src/main/java/com/project/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/test/hello")
+	public String helloWorld() {
+		return "hello";
+		
+	}
+}
